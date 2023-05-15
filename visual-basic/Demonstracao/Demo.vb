8e0c15b21ee7eddb501040505d32e15f4b4ef3d3
KevinAssis <81377237+KevinAssis@users.noreply.github.com>
2023-05-15 19:10:56 -0300

    chore(demonstracao): Adicionando algumas informações a mais na saída da demonstração.

--- visual-basic/Demonstracao/Demo.vb
+++ visual-basic/Demonstracao/Demo.vb
@@ -16,6 +16,12 @@ Module Demo
         ' O método chamado para a exibição no console é Arvore.ToString(), que a exibe na horizontal.
         ' Caso a janela do console seja pequena, modifique o zoom ou o tamanho e aperte r para exibir os resultados novamente.
         ' Outra opção é usar o método ToStringVertical, que ocupa menos espaço na horizontal, mas o resultado é muito menos legível.
+
+        Console.WriteLine(
+            "Essa é uma demonstração do funcionamento da árvore." + NL +
+            "São adicionados e depois removidos 20 números entre 0 e 99 a um objeto Arvore(Of Integer)."
+        )
+
         Dim Resposta = ConsoleKey.R
 
         While Resposta = ConsoleKey.R
@@ -31,7 +37,7 @@ Module Demo
             Console.WriteLine(
                 "Aperte R para executar novamente." + NL +
                 "Qualquer outra tecla para terminar o programa." + NL +
-                "Caso a formatação esteja confusa redimensione a janela e aperte R."
+                "Caso a formatação esteja confusa redimensione a janela e aperte R." + NL
             )
 
             Resposta = Console.ReadKey().Key
@@ -57,13 +63,18 @@ Module Demo
     ''' <param name="Numeros">Array de Integer a ser colocado a árvore.</param>
     ''' <returns>Arvore(Of Integer) contendo os elementos do array.</returns>
     Friend Function ArrayParaArvore(Numeros As Integer()) As Arvore(Of Integer)
+
         Dim Arv As New Arvore(Of Integer)
+        Console.WriteLine(NL + "Adicionando os números a uma árvore vazia." + NL)
+
         For Each N In Numeros
             Console.WriteLine($"Inserindo {N}{NL}")
             Arv.Inserir(N)
             Console.WriteLine(Arv)
         Next
+
         Return Arv
+
     End Function
 
     ''' <summary>
@@ -72,11 +83,15 @@ Module Demo
     ''' <param name="Numeros">Array de Integer contendo os elementos a serem removidos.</param>
     ''' <param name="Arv">Arvore(Of Integer) de onde devem ser removidos os elementos.</param>
     Friend Sub RemoverListaArvore(Numeros As Integer(), Arv As Arvore(Of Integer))
+
+        Console.WriteLine("Removendo os mesmo números da árvore." + NL)
+
         For Each N In Numeros
             Console.WriteLine($"Removendo {N}{NL}")
             Arv.Remover(N)
             Console.WriteLine(Arv)
         Next
+
     End Sub
 
 End Module
